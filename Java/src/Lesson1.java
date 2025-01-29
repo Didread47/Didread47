626e902c7f0aa539eee8a4734605e3f21a9b4fd0
Gusev Evgeniy <didread47@yandex.ru>
2025-01-29 21:15:56 +0300

    hello

---
+++ Java/src/Lesson1.java
@@ -0,0 +1,68 @@
+public class Lesson1 {
+    public static void main (String[] args){
+
+        byte a1 = 12;
+        byte a2 = 014;
+        byte a3 = 0b1100;
+        byte a4 = 0xC;
+        short b1 = 1300;
+        short b2 = 02424;
+        short b3 = 0b0101_0001_0100;
+        short b4 = 0x514;
+        int c1 = 0;
+        int c2 = 00;
+        int c3 = 0b0;
+        int c4 = 0x0;
+        long d1 = 123456789;
+        long d2 = 0726_746_425;
+        long d3 = 0b01110101_1011_1100_1101_0001_0101L;
+        long d4 = 0x75B_CD15;
+        boolean e1 = true;
+        boolean e2 = false;
+        float f1 = 4.2F;
+        float f2 = 4.20F;
+        double g1 = 12.12d;
+        double g2 = 100.100d;
+
+        char h1 = 'z';
+        char h2 = '\u1001';
+        char h3 = 666;
+
+
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+        System.out.println(a4);
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(d3);
+        System.out.println(d4);
+
+        System.out.println(e1);
+        System.out.println(e2);
+
+        System.out.println(g1);
+        System.out.println(g2);
+
+        System.out.println(h1);
+        System.out.println(h2);
+        System.out.println(h3);
+
+
+    }
+
+
+
+}
+
