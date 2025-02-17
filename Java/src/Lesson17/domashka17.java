f292ac23c224053e1a94267d5e73b395464e435e
Gusev Evgeniy <didread47@yandex.ru>
2025-02-17 08:28:30 +0300

    trash...

---
+++ Java/src/Lesson17/domashka17.java
@@ -0,0 +1,23 @@
+package Lesson17;
+
+import java.util.Arrays;
+
+public class domashka17 {
+
+    public static boolean ravenstvo(StringBuilder st, StringBuilder st1){
+    st.toString();
+    st1.toString();
+    return true;
+    }
+
+    public static void main(String[] args) {
+        StringBuilder st = new StringBuilder("Привет друг!");
+        StringBuilder st1 = new StringBuilder("Привет друг!");
+        StringBuilder st3 = new StringBuilder("Привет подруга!");
+        System.out.println(st.toString().contentEquals(st));
+        System.out.println(st.toString().contentEquals(st1));
+        System.out.println(st.toString().contentEquals(st3));
+
+    }
+}
+
